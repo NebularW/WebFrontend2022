5c73daa2bb2be707cc7adf696c46c119c4cd8858
Nebular <73105465+NebularW@users.noreply.github.com>
2023-01-16 17:43:34 +0800

    add style

--- hw6/main.js
+++ hw6/main.js
@@ -17,9 +17,9 @@
 
 import 'babel-polyfill';
 import * as tf from '@tensorflow/tfjs';
+import links from './links';
 
 tf.ENV.set('WEBGL_PACK', false);  // This needs to be done otherwise things run very slow v1.0.4
-import links from './links';
 
 /**
  * Main application to start on window load
@@ -81,7 +81,7 @@ class Main {
     async loadMobileNetStyleModel() {
         if (!this.mobileStyleNet) {
             this.mobileStyleNet = await tf.loadGraphModel(
-                'saved_model_style_js/model.json');
+                'model/saved_model_style_js/model.json');
         }
 
         return this.mobileStyleNet;
@@ -90,7 +90,7 @@ class Main {
     async loadInceptionStyleModel() {
         if (!this.inceptionStyleNet) {
             this.inceptionStyleNet = await tf.loadGraphModel(
-                'saved_model_style_inception_js/model.json');
+                'model/saved_model_style_inception_js/model.json');
         }
 
         return this.inceptionStyleNet;
@@ -99,7 +99,7 @@ class Main {
     async loadOriginalTransformerModel() {
         if (!this.originalTransformNet) {
             this.originalTransformNet = await tf.loadGraphModel(
-                'saved_model_transformer_js/model.json'
+                'model/saved_model_transformer_js/model.json'
             );
         }
 
@@ -109,7 +109,7 @@ class Main {
     async loadSeparableTransformerModel() {
         if (!this.separableTransformNet) {
             this.separableTransformNet = await tf.loadGraphModel(
-                'saved_model_transformer_separable_js/model.json'
+                'model/saved_model_transformer_separable_js/model.json'
             );
         }
 
@@ -156,8 +156,7 @@ class Main {
             hiddenCanvas.height = this.webcamVideoElement.height;
             hiddenContext.drawImage(this.webcamVideoElement, 0, 0,
                 hiddenCanvas.width, hiddenCanvas.height);
-            const imageDataURL = hiddenCanvas.toDataURL('image/jpg');
-            element.src = imageDataURL;
+            element.src = hiddenCanvas.toDataURL('image/jpg');
             this.camModal.modal('hide');
         };
     }
@@ -221,15 +220,15 @@ class Main {
     }
 
     parser() {
-        var i = 0;
+        let i = 0;
         for (i = 0; i < location.search.length; ++i) {
             // console.log(location.search.charAt(i));
-            if (location.search.charAt(i) == '=') {
+            if (location.search.charAt(i) === '=') {
                 break;
             }
         }
         ++i;
-        var pic = 0;
+        let pic = 0;
         for (; i < location.search.length; ++i) {
             pic = pic * 10 + (location.search.charAt(i) - '0');
         }
@@ -348,7 +347,8 @@ class Main {
             const randomNumber = Math.floor(Math.random() * links.length);
             element.src = links[randomNumber];
         } else {
-            element.src = 'images/' + selectedValue + '.jpg';
+            element.src = 'assets/styles/' + selectedValue + '.jpg';
+            console.log(element.src);
         }
     }
 
@@ -371,7 +371,7 @@ class Main {
             const randomNumber = Math.floor(Math.random() * links.length);
             element.src = links[randomNumber];
         } else {
-            element.src = 'image/' + selectedValue + '.jpg';
+            element.src = 'assets/pics/' + selectedValue + '.jpg';
         }
     }
 
@@ -413,7 +413,7 @@ class Main {
             styleBottleneck.dispose();
             identityBottleneck.dispose();
         }
-        this.styleButton.textContent = 'Stylizing image...';
+        this.styleButton.textContent = '图像风格迁移中...';
         await tf.nextFrame();
         const stylized = await tf.tidy(() => {
             return this.transformNet.predict([tf.browser.fromPixels(this.contentImg).toFloat().div(tf.scalar(255)).expandDims(), bottleneck]).squeeze();
@@ -454,70 +454,6 @@ class Main {
         combinedBottleneck.dispose();
         stylized.dispose();
     }
-
-    async benchmark() {
-        const x = tf.randomNormal([1, 256, 256, 3]);
-        const bottleneck = tf.randomNormal([1, 1, 1, 100]);
-
-        let styleNet = await this.loadInceptionStyleModel();
-        let time = await this.benchmarkStyle(x, styleNet);
-        styleNet.dispose();
-
-        styleNet = await this.loadMobileNetStyleModel();
-        time = await this.benchmarkStyle(x, styleNet);
-        styleNet.dispose();
-
-        let transformNet = await this.loadOriginalTransformerModel();
-        time = await this.benchmarkTransform(
-            x, bottleneck, transformNet);
-        transformNet.dispose();
-
-        transformNet = await this.loadSeparableTransformerModel();
-        time = await this.benchmarkTransform(
-            x, bottleneck, transformNet);
-        transformNet.dispose();
-
-        x.dispose();
-        bottleneck.dispose();
-    }
-
-    async benchmarkStyle(x, styleNet) {
-        const profile = await tf.profile(() => {
-            tf.tidy(() => {
-                const dummyOut = styleNet.predict(x);
-                dummyOut.print();
-            });
-        });
-        console.log(profile);
-        const time = await tf.time(() => {
-            tf.tidy(() => {
-                for (let i = 0; i < 10; i++) {
-                    const y = styleNet.predict(x);
-                    y.print();
-                }
-            })
-        });
-        console.log(time);
-    }
-
-    async benchmarkTransform(x, bottleneck, transformNet) {
-        const profile = await tf.profile(() => {
-            tf.tidy(() => {
-                const dummyOut = transformNet.predict([x, bottleneck]);
-                dummyOut.print();
-            });
-        });
-        console.log(profile);
-        const time = await tf.time(() => {
-            tf.tidy(() => {
-                for (let i = 0; i < 10; i++) {
-                    const y = transformNet.predict([x, bottleneck]);
-                    y.print();
-                }
-            })
-        });
-        console.log(time);
-    }
 }
 
 function getRndInteger(min, max) {
@@ -525,7 +461,7 @@ function getRndInteger(min, max) {
 }
 
 window.mobilecheck = function () {
-    var check = false;
+    let check = false;
     (function (a) {
         if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
     })(navigator.userAgent || navigator.vendor || window.opera);
